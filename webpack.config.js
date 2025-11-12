4ccdb6e5819788f395c9e3e8b93d78270a1df940
ann-sed <a.sedeeva@yandex.ru>
2025-11-13 00:00:44 +0300

    Install  webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const config = {
+    mode: 'production', // режим работы webpack - продакшн
+    entry: {  // точки входа для сборки
+        index: './src/js/index.js',
+        // contacts: './src/js/contacts.js',
+    },
+    output: {
+        filename: '[name].bundle.js', // в [name] - это placeholder, сюда будет попадать имя файла в зависимости от файла js
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // здесь указываются модули, которые мы подключили для обработки стилей
+            },
+        ],
+    },
+};
+
+module.exports = config;
