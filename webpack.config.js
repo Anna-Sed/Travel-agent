f5d3cc410c52826d77db0d9aa85fbd4c0caa9b9f
ann-sed <a.sedeeva@yandex.ru>
2025-11-18 00:40:16 +0300

    Set up a multi-page website

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const config = {
     mode: 'production', // режим работы webpack - продакшн
     entry: {  // точки входа для сборки
         index: './src/js/index.js',
-        // contacts: './src/js/contacts.js',
+        contacts: './src/js/contacts.js',
     },
     output: {
         filename: '[name].bundle.js', // в [name] - это placeholder, сюда будет попадать имя файла в зависимости от файла js
