58e36ff7a398f268dbf834b91270ebe8c718d0a5
ann-sed <a.sedeeva@yandex.ru>
2025-11-20 20:21:49 +0300

    Enable automatic sass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
+const sassGlob = require('gulp-sass-glob');
 const browserSync = require('browser-sync').create();
 const clean = require('gulp-clean');
 const fs = require('fs');
@@ -56,6 +57,7 @@ gulp.task('sass', function() {
         .pipe(changed('./dist/css/'))
         .pipe(plumber(plumberNotify('SCSS')))
         .pipe(sourceMaps.init())
+        .pipe(sassGlob())
         .pipe(sass())
         // .pipe(groupMedia())
         .pipe(sourceMaps.write())
