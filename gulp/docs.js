9f372e43b1e7c8d2859bf7a0f459dafba690de58
ann-sed <a.sedeeva@yandex.ru>
2025-11-20 23:00:18 +0300

    Add gulp-autoprefixer

--- gulp/docs.js
+++ gulp/docs.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
+
+// SASS
 const sass = require('gulp-sass')(require('sass'));
 const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+
 const browserSync = require('browser-sync').create();
 const clean = require('gulp-clean');
 const fs = require('fs');
 const sourceMaps = require('gulp-sourcemaps');
+// Во время разработки лучше не использовать, так как перезаписываются стили и всё едет
+// Не работает sourceMaps так как следовало бы. Полезнее groupMedia в продакшене, когда всё сжимается и оптимизируется
+const groupMedia = require('gulp-group-css-media-queries');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const webpack = require('webpack-stream');
@@ -15,12 +22,12 @@ const imagemin = require('gulp-imagemin');
 // gulp-changed позволяет gulp обновляет только те файлы, кот. изменились 
 const changed = require('gulp-changed');
 
-gulp.task('clean:dev', function(done) {
-    if(fs.existsSync('./build/')) {
-        return gulp.src('./build/', { read: false }).pipe(clean());
+gulp.task('clean:docs', function(done) {
+    if(fs.existsSync('./docs/')) {
+        return gulp.src('./docs/', { read: false }).pipe(clean());
     }
     done();
-})
+});
 
 
 const fileIncludeSetting = {
@@ -29,77 +36,81 @@ const fileIncludeSetting = {
 }
 
 // Функция возвращает настройку уведомлений об ошибках
-const plumberNotify = (title) => ({
-    errorHandler: notify.onError({
-        title,
-        message: 'Error <%= error.message %>', // вывод сообщения об ошибке. см документацию плагина
-        sound: false //Если будет много ошибок, чтобы они не сыпались все
-    })
-})
+const plumberNotify = (title) => {
+    return {
+        errorHandler: notify.onError({
+            title,
+            message: 'Error <%= error.message %>', // вывод сообщения об ошибке. см документацию плагина
+            sound: false //Если будет много ошибок, чтобы они не сыпались все
+        })
+    }
+};
 
-gulp.task('html:dev', function() {
+gulp.task('html:docs', function() {
     return gulp
     // **/*.html - первый и второй уровень вложенности файлов
     // '!./src/html/blocks/*.html' - маска. Все html файлы из папки blocks включать не нужно. ! - отрицание.
         .src(['./src/html/**/*.html', '!./src/html/blocks/*.html']) 
-        .pipe(changed('./build/'))
+        .pipe(changed('./docs/'))
         .pipe(plumber(plumberNotify('HTML')))
         .pipe(fileInclude(fileIncludeSetting))
-        .pipe(gulp.dest('./build/'))
+        .pipe(gulp.dest('./docs/'))
 });
 
-gulp.task('sass:dev', function() {
+gulp.task('sass:docs', function() {
     return gulp
         .src('./src/scss/*.scss')
-        .pipe(changed('./build/css/'))
+        .pipe(changed('./docs/css/'))
         .pipe(plumber(plumberNotify('SCSS')))
         .pipe(sourceMaps.init())
         .pipe(sassGlob())
+        .pipe(groupMedia()) // Запускать лучше перед sass, тогда исходные карты ломаться не будут.
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(sourceMaps.write())
-        .pipe(gulp.dest('./build/css/'))
+        .pipe(gulp.dest('./docs/css/'))
 })
 
-gulp.task('copyImg:dev', function() {
+gulp.task('copyImg:docs', function() {
     return gulp
         .src('./src/img/**/*')
-        .pipe(changed('./build/img/'))
+        .pipe(changed('./docs/img/'))
         //verbose: true - это настройка, благодаря кот. при оптимизации мы увидем,
         // какие файлы оптимизированны и сколько место оптимизир.
-        // .pipe(imagemin({ verbose: true }))
-        .pipe(gulp.dest('./build/img/'))
+        .pipe(imagemin({ verbose: true }))
+        .pipe(gulp.dest('./docs/img/'))
 })
 
 // Копирование наших шрифтов в папку dist
-gulp.task('fonts:dev', function() {
+gulp.task('fonts:docs', function() {
     return gulp
         .src('./src/fonts/**/*')
-        .pipe(changed('./build/fonts/'))
-        .pipe(gulp.dest('./build/fonts/'))
+        .pipe(changed('./docs/fonts/'))
+        .pipe(gulp.dest('./docs/fonts/'))
 })
 
 // Копирование доп файлов для пользователя в папку dis 
 // (например пфки и какие-то документы, чтобы пользовотель их скачивал с сайта)
-gulp.task('files:dev', function() {
+gulp.task('files:docs', function() {
     return gulp
         .src('./src/files/**/*')
-        .pipe(changed('./build/files/'))
-        .pipe(gulp.dest('./build/files/'))
+        .pipe(changed('./docs/files/'))
+        .pipe(gulp.dest('./docs/files/'))
 })
 
-gulp.task('js:dev', function() {
+gulp.task('js:docs', function() {
     return gulp
         .src('./src/js/*.js')
-        .pipe(changed('./build/js'))
+        .pipe(changed('./docs/js'))
         .pipe(plumber(plumberNotify('JS')))
-        // .pipe(babel()) Нужен для старых браузеров
+        .pipe(babel())
         .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('./build/js'))
+        .pipe(gulp.dest('./docs/js'))
 });
 
 const serverOption = {
     server: {
-        baseDir: './build/' // Папка, которую нужно обслуживать
+        baseDir: './docs/' // Папка, которую нужно обслуживать
     },
     open: true,          // Автоматически открывать браузер
     port: 8000,         // Порт для сервера
@@ -108,16 +119,6 @@ const serverOption = {
     }
 };
 
-gulp.task('server:dev', function() {
+gulp.task('server:docs', function() {
     browserSync.init(serverOption);
-})
-
-// Говорим, чтобы gulp отслеживал все изменения в определенных файлах, папках и запускал таски на эти изменения.
-gulp.task('watch:dev', function() {
-    gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass:dev'));
-    gulp.watch('./src/**/*.html', gulp.parallel('html:dev'));
-    gulp.watch('./src/img/**/*', gulp.parallel('copyImg:dev'));
-    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts:dev'));
-    gulp.watch('./src/files/**/*', gulp.parallel('files:dev'));
-    gulp.watch('./src/js/**/*.js', gulp.parallel('js:dev'));
 })
