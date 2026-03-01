de9b2b928da8fb62b9f74a210a99a108b709832d
ann-sed <a.sedeeva@yandex.ru>
2026-03-01 19:12:03 +0300

    Connect autoComplete in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+import autoComplete from "@tarekraafat/autocomplete.js";
+import countryList from "./helpers/countryList";
+
+const autoCompleteJS = new autoComplete({
+    selector: "#autoCompleteCountry",
+    placeHolder: "e.g Bali, Indonesia",
+    data: {
+        src: countryList,
+    },
+    resultItem: {
+        highlight: true,
+    },
+    events: {
+        input: {
+            selection: (event) => {
+                const selection = event.detail.selection.value;
+                autoCompleteJS.input.value = selection;
+            },
+        },
+    },
+    resultsList: {
+        maxResults: 10,
+    },
+    searchEngine: 'strict',
+});
+
+
+
