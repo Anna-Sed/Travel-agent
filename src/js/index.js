25736ca44e6491368fcefeb80d36459e14af8911
ann-sed <a.sedeeva@yandex.ru>
2026-03-14 21:32:19 +0300

    Split into modules packages in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,15 @@
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
-import { easepick, TimePlugin } from '@easepick/bundle';
+import easepick from './modules/easepick.js';
+import getAutoComlete from './modules/autoComplete.js';
+import scrollReveal from './modules/scrollReveal.js';
+import swiper from './modules/swiper.js';
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+swiper();
 
-import getAutoComlete from './modules/autoComplete.js'
-
-// Первый аргумент - селектор по которому мы находим элемент - обёртку, на который вешается свайпер
-// Второй аргумент - объект с настройками
-const swiper = new Swiper('.swiper', {
-    // Configure swiper to use modules
-    slidesPerView: 4,
-    spaceBetween: 32,
-    navigation: {
-        prevEl: '#sliderPrev',
-        nextEl: '#sliderNext',
-    },
-   
-});
-
-const picker = new easepick.create({
-    element: document.getElementById('datePicket'),
-    css: [
-        'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css',
-        'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
-    ],
-    format: 'HH:mm, DD/MM/YY',
-    plugins: [TimePlugin],
-});
+easepick();
 
 getAutoComlete();
+
+scrollReveal();
