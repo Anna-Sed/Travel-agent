326a8fa994300d1aab028dc4561d7cbb45fba742
ann-sed <a.sedeeva@yandex.ru>
2026-03-14 21:01:46 +0300

    Add easepick.js module

---
+++ src/js/modules/easepick.js
@@ -0,0 +1,15 @@
+import { easepick, TimePlugin } from '@easepick/bundle';
+
+function getPicker () {
+    const picker = new easepick.create({
+        element: document.getElementById('datePicket'),
+        css: [
+            'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css',
+            'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css',
+        ],
+        format: 'HH:mm, DD/MM/YY',
+        plugins: [TimePlugin],
+    });
+};
+
+export default getPicker;
