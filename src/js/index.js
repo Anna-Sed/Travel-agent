cf9b5cdbd440078af8e127be9e8edc7a20b18e6d
ann-sed <a.sedeeva@yandex.ru>
2026-04-06 22:51:05 +0300

    Switch off the scrollReveal and plug the form.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
 
 import easepick from './modules/easepick.js';
 import getAutoComlete from './modules/autoComplete.js';
-import scrollReveal from './modules/scrollReveal.js';
+// import scrollReveal from './modules/scrollReveal.js';
 import swiperDirection from './modules/swiper-direction.js';
 import swiperTestimonials from './modules/swiper-testimonials.js';
+import getSubscribeFormData from './modules/form.js';
 
 swiperDirection();
 swiperTestimonials();
@@ -14,4 +15,9 @@ easepick();
 
 getAutoComlete();
 
-scrollReveal();
+// Вызываем функцию после загрузки DOM
+document.addEventListener('DOMContentLoaded', () => {
+    getSubscribeFormData();
+});
+
+// scrollReveal();
