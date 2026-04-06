3fdcef2cf2bd89c259c6e10cfe786bef2e2b3676
ann-sed <a.sedeeva@yandex.ru>
2026-04-06 22:50:05 +0300

    Update the src/js/modules/mobile-nav.js

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,17 @@
 const mobileNav = () => {
-//     // mobile nav button
-//     const navBtn = document.querySelector('.mobile-nav-btn');
-//     const nav = document.querySelector('.mobile-nav');
-//     const menuIcon = document.querySelector('.nav-icon');
+    // mobile nav button
+    const navBtn = document.querySelector('.mobile-nav-btn');
+    const nav = document.querySelector('.mobile-nav');
+    const menuIcon = document.querySelector('.nav-icon');
 
-//     navBtn.addEventListener('click', () => {
-//         nav.classList.toggle('mobile-nav--open');
-//         menuIcon.classList.toggle('nav-icon--active'); // Превращает иконку в крестик
-//         document.body.classList.toggle('no-scroll')
-//     });
+    navBtn.addEventListener('click', () => {
+        // Переключаем класс для меню
+        nav.classList.toggle('mobile-nav--open');
+        // Переключаем класс для иконки (полоски ↔ крестик)
+        menuIcon.classList.toggle('nav-icon--active'); // Превращает иконку в крестик
+        // Блокируем прокрутку страницы при открытом меню
+        document.body.classList.toggle('no-scroll')
+    });
 }
 
 export default mobileNav;
